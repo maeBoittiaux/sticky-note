c63e9f237e128ab7de19a5c9645519afc4c832a6
maebo <maeboittiaux@gmail.com>
2024-08-31 14:10:58 +0100

    feat (App.js) : add habit list component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import HabitList from './components/HabitList';
 
 function App() {
   return (
     <div className="App">
       <h1>Habit Tracker</h1>
+      <HabitList />
     </div>
   );
 }
